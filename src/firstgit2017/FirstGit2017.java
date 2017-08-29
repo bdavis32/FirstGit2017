a4f294901ad001aa8c60daaca2f497f4cc49e48e
Benjamin <Benjamin@10.30.18.236>
2017-08-29 15:35:08 -0500

    First commit

---
+++ src/firstgit2017/FirstGit2017.java
@@ -0,0 +1,16 @@
+package firstgit2017;
+
+/**
+ *
+ * @author Benjamin
+ */
+public class FirstGit2017 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
