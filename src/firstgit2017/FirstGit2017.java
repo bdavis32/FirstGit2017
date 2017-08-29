4c586452f71f433c648db531c6e243a98e8f5f03
Benjamin <Benjamin@10.30.18.236>
2017-08-29 15:43:34 -0500

    Added statement to main

--- src/firstgit2017/FirstGit2017.java
+++ src/firstgit2017/FirstGit2017.java
@@ -11,6 +11,7 @@ public class FirstGit2017 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Second comment");
     }
     
 }
